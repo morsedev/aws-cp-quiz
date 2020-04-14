e22d3526de34e5a4660b82ee9ab3c07f121bbf2e
Braddy Yeoh <braddy.yeoh@ucdconnect.ie>
2020-04-14 16:11:47 +0100

    answer color is now blue

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const AnswerP = styled.p.attrs({
-    className: `fw3 f3 green`
+    className: `fw3 f3 blue`
 })`
     display: ${props => props.selected ? "inline" : "none"}
 `
